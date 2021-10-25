7bca03474d42c99ad7a972aded2bb3d4365aab03
Vitor Amorim <vitor232596.job@gmail.com>
2021-10-25 17:17:38 -0300

    branch vitor - Iniciando tela de cadastro de Enderço

---
+++ app/src/main/java/com/example/aplicativoconfeitaria/model/Endereco.java
@@ -0,0 +1,82 @@
+package com.example.aplicativoconfeitaria.model;
+
+public class Endereco {
+    private String idUsuario;
+    private String cep,descricao,endereco,bairro,cidade,referencia,complemento;
+    private Integer numero;
+
+    public Endereco() {
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
